8f09707b9529b59976544060f075c409ac1d9d43
DAIT <daitdede@gmail.com>
2026-02-19 19:17:34 -0600

    Fix syntax error in SaludoController class
    Test para examen

--- src/Controller/Api/SaludoController.php
+++ src/Controller/Api/SaludoController.php
@@ -7,7 +7,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class SaludoController extends AbstractController
+class SaludoController extends AbstractController;
 {
     #[Route('/api/saludo', name: 'api_saludo', methods: ['POST'])]
     public function saludo(Request $request): JsonResponse
